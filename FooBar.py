8aff8bf6cf8c1ddc0bd378d71d829daaa81f2803
AnthonyKot <kotatut@gmail.com>
2021-04-12 12:06:21 +0200

    Update FooBar.py

--- FooBar.py
+++ FooBar.py
@@ -10,8 +10,7 @@ for i in range(0, 101, 5):
     print(i, end=" ")
     
 print("\n\nCounting Foobar: ") 
-for i in range(0, 101):
-    if i % 3 == 0 and i % 5 == 0:
-      print(i,end = " ")
+for i in range(0, 101, 15): #!
+    print(i,end = " ")
       
 input("\n\nPress the enter key to exit.\n")
